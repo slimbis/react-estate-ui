d2edc5ea33b0f73b7cf8b2510f82c9cd264e696e
slimbis <limberopoulos@gmail.com>
2024-06-19 22:22:23 +0300

    initial css

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./navbar.scss";
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                <img src="/logo.png" alt="" className="img" />
+                <span>
+                    Deep Insights RE
+                </span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">Sign in</a>
+                <a href="/" className="register">Sign up</a>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Navbar;
